90f99f626c51edf52868bcb924d74f6a5e8f6f0e
Vysakhdn <vysakhdn123@gmail.com>
2023-08-31 15:53:52 +0530

    traffic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const TRAFFIC_LIGHT_STATES = {
+  STOP: 'stop',
+  READY: 'ready',
+  GO: 'go'
+};
+
 function App() {
+  const [currentLight, setCurrentLight] = useState(TRAFFIC_LIGHT_STATES.STOP);
+  const [isAutoMode, setIsAutoMode] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isAutoMode) {
+      intervalId = setInterval(() => {
+        setCurrentLight((prevLight) => {
+          switch (prevLight) {
+            case TRAFFIC_LIGHT_STATES.STOP:
+              return TRAFFIC_LIGHT_STATES.READY;
+            case TRAFFIC_LIGHT_STATES.READY:
+              return TRAFFIC_LIGHT_STATES.GO;
+            case TRAFFIC_LIGHT_STATES.GO:
+              return TRAFFIC_LIGHT_STATES.STOP;
+            default:
+              return prevLight;
+          }
+        });
+      }, 10000); // Change light every 10 seconds in auto mode
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isAutoMode]);
+
+  const handleManualChange = (light) => {
+    setCurrentLight(light);
+    setIsAutoMode(false);
+  };
+
+  const handleAutoModeToggle = () => {
+    setIsAutoMode((prevAutoMode) => !prevAutoMode);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`traffic-light ${currentLight} ${isAutoMode ? 'blinking' : ''}`}>
+        <div className="light red"></div>
+        <div className="light yellow"></div>
+        <div className="light green"></div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleManualChange(TRAFFIC_LIGHT_STATES.STOP)}>Stop</button>
+        <button onClick={() => handleManualChange(TRAFFIC_LIGHT_STATES.READY)}>Ready</button>
+        <button onClick={() => handleManualChange(TRAFFIC_LIGHT_STATES.GO)}>Go</button>
+        <button onClick={handleAutoModeToggle}>Auto</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
